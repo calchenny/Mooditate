b0efa3f9c189962b7a323486b2cf7cd4acdeaf3d
Calvin Chen <calvinchen345@gmail.com>
2021-01-17 09:59:14 -0800

    v2.1

--- countdown.js
+++ countdown.js
@@ -3,35 +3,7 @@ var countDownTime = 0;
 function timeSelected(num){
     countDownTime = num;
     document.getElementById("demo").innerHTML = countDownTime;
+    
 }
 
 
-// Update the count down every 1 second
-function startMeditation(){
-var x = setInterval(function() {
-    document.getElementById("CDtime").innerHTML = countDownTime;
-    $.get( "countdown.html", null, function(text) {
-      $( ".result" ).html( data );
-      alert( "Load was performed." );
-    
-    // Get today's date and time
-    var now = new Date().getTime();
-    document.getElementById("CDtime").innerHTML = now;
-    var newTime = new Date(now.getTime() + countDownTime*60000);
-    // Find the distance between now and the count down date
-    var distance = countDownTime - now;
-    // Time calculations for days, hours, minutes and seconds
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-    // Output the result in an element with id="demo"
-    document.getElementById("CDtime").innerHTML = minutes + "m " + seconds + "s ";
-  });
-    // If the count down is over, write some text 
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("CDtime").innerHTML = "Your meditation is over";
-    }
-    
-  }, 1000);
-}
--- testCD.js
+++ testCD.js
@@ -1,12 +1,13 @@
-var countDownTime = 10;
+const countDownTime = 10;
 let time = countDownTime * 60;
 
 const countdownEl = document.getElementById('CDtime');
+
 setInterval(updateCD, 1000);
-function updateCD(){
+function updateCD() {
   const minutes = Math.floor(time / 60);
   let seconds = time % 60;
-  seconds = seconds < 10 : '0' + seconds : seconds;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
   countdownEl.innerHTML = `${minutes}: ${seconds}`;
   time--;
 }
