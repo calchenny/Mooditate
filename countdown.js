766daba90383d2d316be3474746c879a1b2ce380
Calvin Chen <calvinchen345@gmail.com>
2021-01-17 09:50:41 -0800

    v2.1

--- countdown.js
+++ countdown.js
@@ -4,21 +4,7 @@ function timeSelected(num){
     countDownTime = num;
     document.getElementById("demo").innerHTML = countDownTime;
 }
-var x = setInterval(function() {
-  document.getElementById("demo").innerHTML = countDownTime;
-  // Get today's date and time
-  var now = new Date().getTime();
-  document.getElementById("demo").innerHTML = now;
-  var newTime = new Date(now.getTime() + countDownTime*60000);
-  // Find the distance between now and the count down date
-  var distance = countDownTime - now;
-  // Time calculations for days, hours, minutes and seconds
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-  // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
-});
+
 
 // Update the count down every 1 second
 function startMeditation(){
