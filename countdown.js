a5537650130ccc5da14a48b3e11a7fe5148de737
Calvin Chen <calvinchen345@gmail.com>
2021-01-17 11:32:06 -0800

    v2.55

--- countdown.js
+++ countdown.js
@@ -1,13 +1,15 @@
-const countDownTime = 10;
 
-    let time = countDownTime * 60;
-    const countdownEl = document.getElementById('CDtime');
-    setInterval(updateCD, 1000);
+const countDownTime = localStorage.getItem(countDownTime);
+document.getElementById('CDTime').innerHTML = countDownTime;
 
-    function updateCD() {
-      const minutes = Math.floor(time / 60);
-      let seconds = time % 60;
-      seconds = seconds < 10 ? '0' + seconds : seconds;
-      countdownEl.innerHTML = `${minutes}: ${seconds}`;
-      time--;
-  }
+  //   let time = countDownTime * 60;
+  //   const countdownEl = document.getElementById('CDtime');
+  //   setInterval(updateCD, 1000);
+
+  //   function updateCD() {
+  //     const minutes = Math.floor(time / 60);
+  //     let seconds = time % 60;
+  //     seconds = seconds < 10 ? '0' + seconds : seconds;
+  //     countdownEl.innerHTML = `${minutes}: ${seconds}`;
+  //     time--;
+  // }
--- meditate.js
+++ meditate.js
@@ -1,31 +1,32 @@
 // gets the time selected by the user
-
 var countDownTime = 0;
-
 function timeSelected(num){
     countDownTime = num;
+    localStorage.setItem('countDownTime' , 'num');  
     document.getElementById("demo").innerHTML = countDownTime;
+    // test = localStorage.getItem('countDownTime');
+    // document.getElementById('demo2').innerHTML = "test: " test;  
 }
 
 function clicked(){
-    document.getElementById("startCD").value = countDownTime;
-    var x = document.getElementById("startCD").getAttribute("value");
-    document.getElementById("demo").innerHTML = "CD is " + x;
+    // document.getElementById("startCD").value = countDownTime;
+    // var x = document.getElementById("startCD").getAttribute("value");
+    //localStorage.setItem(countDownTime, CDMin);
     window.location.href = "countdown.html";
-    const startingTime = countDownTime;
-    let time = startingTime * 60;
-    document.getElementById('CDtime').innerHTML = "hello";
+    // const startingTime = countDownTime;
+    // let time = startingTime * 60;
+    // document.getElementById('CDtime').innerHTML = "hello";
     // const countdownEl = document.getElementById('CDtime');
     // setInterval(updateCD(time, countdownEl), 1000);
 }
 
-function updateCD(t, cd) {
-    const minutes = Math.floor(t / 60);
-    let seconds = t % 60;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    cd.innerHTML = `${minutes}: ${seconds}`;
-    t--;
-}
+// function updateCD(t, cd) {
+//     const minutes = Math.floor(t / 60);
+//     let seconds = t % 60;
+//     seconds = seconds < 10 ? '0' + seconds : seconds;
+//     cd.innerHTML = `${minutes}: ${seconds}`;
+//     t--;
+// }
 
 // document.getElementById("startCD").value = countDownTime;
 
