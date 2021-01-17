e1a5bfd78f4434202581e581ab3ea7ff89453ad6
Calvin Chen <calvinchen345@gmail.com>
2021-01-17 10:30:19 -0800

    v2.1

--- countdown.js
+++ countdown.js
@@ -1,9 +1,19 @@
-// gets the time selected by the user
+
 var countDownTime = 0;
-function timeSelected(num){
+
+function selectedTime(num){
     countDownTime = num;
-    document.getElementById("demo").innerHTML = countDownTime;
-    
 }
 
+let time = countDownTime * 60;
 
+const countdownEl = document.getElementById('CDtime');
+
+setInterval(updateCD, 1000);
+function updateCD() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+  countdownEl.innerHTML = `${minutes}: ${seconds}`;
+  time--;
+}
