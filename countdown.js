04f1f5e19cd0650e793007486c85d1e4f7a432a7
Calvin Chen <calvinchen345@gmail.com>
2021-01-17 10:55:22 -0800

    v2.3

--- countdown.js
+++ countdown.js
@@ -1,19 +1,1 @@
 
-var countDownTime = getElementById('startCD').getAttribute('value');
-
-function selectedTime(num){
-    countDownTime = num;
-}
-
-let time = countDownTime * 60;
-
-const countdownEl = document.getElementById('CDtime');
-
-setInterval(updateCD, 1000);
-function updateCD() {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-  seconds = seconds < 10 ? '0' + seconds : seconds;
-  countdownEl.innerHTML = `${minutes}: ${seconds}`;
-  time--;
-}
--- meditate.js
+++ meditate.js
@@ -7,10 +7,22 @@ function timeSelected(num){
     document.getElementById("demo").innerHTML = countDownTime;
 }
 
+function updateCD(time, countdownEl) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
+
 function clicked(){
     document.getElementById("startCD").value = countDownTime;
     var x = document.getElementById("startCD").getAttribute("value");
     document.getElementById("demo").innerHTML = "CD is " + x;
+    window.location.href = "countdown.html";
+    let time = countDownTime * 60;
+    const countdownEl = document.getElementById('CDtime');
+    setInterval(updateCD(time, countdownEl), 1000);
 }
 
 // document.getElementById("startCD").value = countDownTime;
@@ -19,4 +31,15 @@ function clicked(){
 
 // document.getElementById("startCD").onclick = "selectedTime(" + countDownTime + ")";
 // document.getElementById("demo").innerHTML = "CD time" + test;
+
+// var countDownTime = getElementById('startCD').getAttribute('value');
+
+// function selectedTime(num){
+//     countDownTime = num;
+// }
+
+
+
+
+
 
