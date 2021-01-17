e1c2ab540fa71506da71e6ce7835b4d0239d323b
Calvin Chen <calvinchen345@gmail.com>
2021-01-17 13:10:33 -0800

    v2.81

--- countdown.js
+++ countdown.js
@@ -2,6 +2,7 @@ var CD = localStorage.getItem('startingCD');
 let time = CD * 60;
 const countdownEl = document.getElementById('CDtime');
 setInterval(updateCD, 1000);
+
 function updateCD() {
   const minutes = Math.floor(time / 60);
   let seconds = time % 60;
--- main.js
+++ main.js
@@ -7,3 +7,22 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
+
+function onSignIn(googleUser){
+  console.log(googleUser.getBasicProfile())
+  var profile = googleUser.getBasicProfile()
+  console.log('User is ' + JSON.stringify(profile))
+
+  var element = document.querySelector('#content')
+  element.innerText = profile.getGivenName();
+
+  var image = document.createElement('img')
+  image.setAttribute('src', profile.getImageurl())
+  element.append(image)
+}
+
+function signOut(){
+  gapi.auth2.getAuthInstance().signOut().then(function(){
+    console.log('user signed out')
+  })
+}
